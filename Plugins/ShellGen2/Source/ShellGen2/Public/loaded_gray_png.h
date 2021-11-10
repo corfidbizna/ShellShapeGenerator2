ce763f8789dd7389d4a99517e390b525d13c7ef1
Solra Bizna <solra@bizna.name>
2021-11-10 15:07:20 -0700

    Handle negative U/V coordinates in the distortion

--- Plugins/ShellGen2/Source/ShellGen2/Public/loaded_gray_png.h
+++ Plugins/ShellGen2/Source/ShellGen2/Public/loaded_gray_png.h
@@ -4,6 +4,17 @@
 #include <cmath>
 #include <memory>
 
+namespace {
+  // modular modulo, assuming q is positive
+  int32_t umod(int32_t d, int32_t q) {
+    if(d < 0) {
+      auto ret = q - -d % q;
+      if(ret == q) return 0;
+      else return ret;
+    } else return d % q;
+  }
+}
+
 extern const float BYTE_TO_FLOAT[256];
 struct SHELLGEN2_API loaded_gray_png {
   uint32_t width, height;
@@ -21,12 +32,12 @@ struct SHELLGEN2_API loaded_gray_png {
     y = static_cast<int32_t>(vfloor);
     // common subexpression elimination is something I can trust the compiler
     // to do, right? ...right?
-    auto row1 = rows[(y)%height];
-    auto row2 = rows[(y+1)%height];
-    float a = BYTE_TO_FLOAT[row1[(x)%width]];
-    float b = BYTE_TO_FLOAT[row1[(x+1)%width]];
-    float c = BYTE_TO_FLOAT[row2[(x)%width]];
-    float d = BYTE_TO_FLOAT[row2[(x+1)%width]];
+    auto row1 = rows[umod(y,height)];
+    auto row2 = rows[umod(y+1,height)];
+    float a = BYTE_TO_FLOAT[row1[umod(x,width)]];
+    float b = BYTE_TO_FLOAT[row1[umod(x+1,width)]];
+    float c = BYTE_TO_FLOAT[row2[umod(x,width)]];
+    float d = BYTE_TO_FLOAT[row2[umod(x+1,width)]];
     float ab = (a * (1.0f - ufract)) + (b * ufract);
     float cd = (c * (1.0f - ufract)) + (d * ufract);
     return (ab * (1.0f - vfract)) + (cd * vfract);
