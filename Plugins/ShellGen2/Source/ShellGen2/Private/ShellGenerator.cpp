828d6b0c9e19a757a6d86cbdc5feb27715e26cfb
corfidbizna <corfidbizna2@gmail.com>
2023-06-01 13:22:35 -0600

    Fill out project information.

--- Plugins/ShellGen2/Source/ShellGen2/Private/ShellGenerator.cpp
+++ Plugins/ShellGen2/Source/ShellGen2/Private/ShellGenerator.cpp
@@ -1,4 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*
+ * This file is part of Shell Shape Generator 2.
+ *
+ * Copyright ©2023 Olivia Jenkins
+ *
+ * Shell Shape Generator 2 is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Shell Shape Generator 2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Shell Shape Generator 2. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "ShellGenerator.h"
 
--- Plugins/ShellGen2/Source/ShellGen2/Private/bnlytmn.cpp
+++ Plugins/ShellGen2/Source/ShellGen2/Private/bnlytmn.cpp
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Shell Shape Generator 2.
+ *
+ * Copyright ©2023 Olivia Jenkins
+ *
+ * Shell Shape Generator 2 is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Shell Shape Generator 2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Shell Shape Generator 2. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "bnlytmn.hpp"
 
 #include <cmath>
@@ -7,7 +26,7 @@
 
 // Shamelessly stealing my own Rust implementation of Bentley-Ottmann, then
 // gutting it to make the creatively named Bnlytmn algorithm. (A more proper
-// name for it would probably be "Reduced Bentley-Ottmann".)
+// name for it would probably be "Reduced Bentley-Ottmann".) -SB
 
 namespace {
   float twice_triangle_area(FVector2D a, FVector2D b, FVector2D c) {
